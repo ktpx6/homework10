2307229ebf151b01961d3e12938fed65922587e9
ktpx6 <ktpx6@bk.ru>
2024-04-21 18:36:59 +0300

    homework 10

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задача 1:");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника — " + fullName);
+        System.out.println();
 
+        System.out.println("Задача 2:");
+        String fullNameOne = "Ivanov Ivan Ivanovich";
+        String uppercaseFullName = fullNameOne.toUpperCase();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + uppercaseFullName);
+        System.out.println();
+
+        System.out.println("Задача 3:");
+        String fullNameTwo = "Иванов Семён Семёнович";
+        String replacedFullName = fullNameTwo.replace("ё","е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + replacedFullName);
     }
 }
